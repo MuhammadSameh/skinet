20b1f42375a02f563495a07e9e311528c7d34067
MuhammadSameh <muhammadsameh970@gmail.com>
2022-06-15 04:32:34 +0200

    Initital Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getproducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+        
+        [HttpGet("getproduct/{num}")]
+        public async Task<Product> GetProduct(int num)
+        {
+            return await _context.Products.FindAsync(num);
+        }
+    }
+}

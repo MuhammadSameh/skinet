e506f49a62c48bbee1a620a76d65c17f6e953c87
MuhammadSameh <muhammadsameh970@gmail.com>
2022-06-21 00:46:24 +0200

    API Architecture

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,23 +5,26 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
+
 
         [HttpGet("getproducts")]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            
+            var products = await _repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -29,7 +32,25 @@ namespace API.Controllers
         [HttpGet("getproduct/{num}")]
         public async Task<Product> GetProduct(int num)
         {
-            return await _context.Products.FindAsync(num);
+            return await _repo.GetProductByIdAsync(num);
+        }
+
+        [HttpGet("getbrands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetBrands()
+        {
+
+            var brands = await _repo.GetBrandsAsync();
+
+            return Ok(brands);
+        }
+        
+        [HttpGet("gettypes")]
+        public async Task<ActionResult<List<ProductType>>> GetTypes()
+        {
+
+            var types = await _repo.GetTypesAsync();
+
+            return Ok(types);
         }
     }
 }
---
+++ Infrastructure/Repositories/ProductRepositry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepositry : IProductRepository
+    {
+        private readonly StoreContext _context;
+
+        public ProductRepositry(StoreContext context )
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetBrandsAsync()
+        {
+            return await _context.productBrands.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            var product = await _context.Products.Include(p => p.ProductBrand).Include(p => p.ProductType).FirstOrDefaultAsync(p=> p.Id == id);
+            return product;
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products.Include(p=> p.ProductBrand).Include(p=>p.ProductType).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetTypesAsync()
+        {
+            return await _context.productTypes.ToListAsync();
+        }
+    }
+}
